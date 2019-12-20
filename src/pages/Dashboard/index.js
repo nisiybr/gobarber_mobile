01468b35c88446d1d3b8ca222918693b0cd9e4d0
nisiybr <guilherme.nisiyama@gmail.com>
2019-12-20 00:45:33 -0300

    Atualizacao

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-
+import { withNavigationFocus } from 'react-navigation';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { Container, Title, List } from './styles';
 import api from '~/services/api';
@@ -7,17 +7,18 @@ import api from '~/services/api';
 import Background from '~/components/Background';
 import Appointment from '~/components/Appointment';
 
-export default function Dashboard() {
+function Dashboard({ isFocused }) {
   const [appointments, setAppointments] = useState([]);
+  async function loadAppointments() {
+    const response = await api.get('appointments');
 
+    setAppointments(response.data);
+  }
   useEffect(() => {
-    async function loadAppointments() {
-      const response = await api.get('appointments');
-
-      setAppointments(response.data);
+    if (isFocused) {
+      loadAppointments();
     }
-    loadAppointments();
-  }, []);
+  }, [isFocused]);
 
   async function handleCancel(id) {
     const response = await api.delete(`appointments/${id}`);
@@ -56,3 +57,5 @@ Dashboard.navigationOptions = {
     <Icon name="event" size={20} color={tintColor} />
   ),
 };
+
+export default withNavigationFocus(Dashboard);
