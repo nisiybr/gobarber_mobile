556d1f30e201abcc98f00c45885be617475bfb4a
nisiybr <guilherme.nisiyama@gmail.com>
2020-01-30 00:09:11 -0300

    AtT

--- android/app/src/main/java/com/modulo_10/MainApplication.java
+++ android/app/src/main/java/com/modulo_10/MainApplication.java
@@ -26,7 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           packages.add(
-            new CodePush(BuildConfig.CODEPUSH_KEY,getApplicationContext(),BuildConfig.DEBUG),
+            new CodePush(BuildConfig.CODEPUSH_KEY,getApplicationContext(),BuildConfig.DEBUG)
             );
           return packages;
         }
